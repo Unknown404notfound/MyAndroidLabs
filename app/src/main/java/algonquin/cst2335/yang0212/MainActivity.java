d4e799f27cd4fe467b4101719b92ae7102aade38
Yuxi Yang <yang0212@algonquinlive.com>
2023-01-11 23:59:57 -0500

    Initial Commit

---
+++ app/src/main/java/algonquin/cst2335/yang0212/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.yang0212;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
