9c9c707cb62de78ede14c60d26ef4ddc159f5447
Yuxi Yang <yang0212@algonquinlive.com>
2023-01-19 18:31:51 -0500

    Adding compound buttons

---
+++ app/src/main/java/algonquin/cst2335/yang0212/ui/MainActivity.java
@@ -0,0 +1,34 @@
+package algonquin.cst2335.yang0212.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import algonquin.cst2335.yang0212.data.MainViewModel;
+import algonquin.cst2335.yang0212.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding variableBinding;
+    private MainViewModel model;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+       // variableBinding.textview.setText(model.editString);
+        variableBinding.mybutton.setOnClickListener(click -> {
+            //model.editString.postValue(variableBinding.myedittext.getText().toString());
+            model.editString.observe(this, s -> {
+                variableBinding.textview.setText("Your edit text has: " + s);
+            });
+        });
+    };
+}
